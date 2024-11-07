e6f7a5e2305ad524f630fe568b90a4f51e2a5960
DiogoCarcau <160623299+DiogoCarcau@users.noreply.github.com>
2024-11-07 08:31:01 +0000

    feito a pagina inicial do javaScript

--- lab7/main.js
+++ lab7/main.js
@@ -2,21 +2,21 @@ function changeColor(color) {
     document.getElementById('paintText').style.color = color;
 }
 
+let counter = 0;
 
-// Função para incrementar o contador
-function incrementCounter() {
+function count(){
     counter++;
-    document.getElementById('counter').textContent = counter;
+    document.getElementById("counter").textContent = counter;
 }
 
 
-function submitColor() {
+function mudaFundo() {
     const color = document.getElementById('colorInput').value; 
     document.body.style.backgroundColor = color; // Altera a cor de fundo da página
 }
 
 // fundo da caixa com cor aleatória
-function mudaBackgroud3() {
+function mudaFundoCaixa() {
     const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#8E44AD', '#1ABC9C'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)]; 
     document.getElementById('inputText').style.backgroundColor = randomColor;
