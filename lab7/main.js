8555aa127ea607e57fdff70815226e81c80c3adc
DiogoCarcau <160623299+DiogoCarcau@users.noreply.github.com>
2024-11-07 08:53:50 +0000

    5 topicos feitos

--- lab7/main.js
+++ lab7/main.js
@@ -9,15 +9,26 @@ function count(){
     document.getElementById("counter").textContent = counter;
 }
 
+function cimaDaFrase(){
+    const mouse=document.getElementById("mouse");
+
+    mouse.addEventListener("mouseover", function(){
+        mouse.textContent="Obrigado por passares!";
+    });
+    mouse.addEventListener("mouseout", function(){
+        mouse.textContent="Passa o mouse aqui!";
+    });
+}
+
 
 function mudaFundo() {
     const color = document.getElementById('colorInput').value; 
-    document.body.style.backgroundColor = color; // Altera a cor de fundo da página
+    document.body.style.backgroundColor = color; 7
 }
 
-// fundo da caixa com cor aleatória
+
 function mudaFundoCaixa() {
-    const colors = ['#FF5733', '#33FF57', '#3357FF', '#F5A623', '#8E44AD', '#1ABC9C'];
+    const colors = ['#FF5733', '#33FF57', '#2257FF', '#F5A623', '#8E44AD', '#1ABC9C'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)]; 
     document.getElementById('inputText').style.backgroundColor = randomColor;
 }
